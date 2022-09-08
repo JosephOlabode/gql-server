8dffa6c65ebba5268ec19924b01fcf12e9c2c0d5
Olabode Joseph Oluwaseun <josepholabode17@gmail.com>
2022-09-08 03:45:20 +0100

    implemented the set up for express graph ql

--- server.js
+++ server.js
@@ -3,3 +3,22 @@ const express_graphql = require('express-graphql');
 const {buildSchema}  = require('graphql');
 
 // GraphQl Schema
+const schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+
+// Root Resolver
+const root = {
+    message: () => 'Hello world!'
+};
+
+// Create an express server and a GraphQL endpoint
+const app = express();
+app.use('/graphql', express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
