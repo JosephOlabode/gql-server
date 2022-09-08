7997a0d20935c6f080dea44d958b7936c3454227
Olabode Joseph Oluwaseun <josepholabode17@gmail.com>
2022-09-08 12:36:00 +0100

    implementing the updating of a course topic

--- server.js
+++ server.js
@@ -1,24 +1,93 @@
 const express = require('express');
-const express_graphql = require('express-graphql').graphqlHTTP;
+const { graphqlHTTP } = require('express-graphql');
 const {buildSchema}  = require('graphql');
 
 // GraphQl Schema
 const schema = buildSchema(`
     type Query {
         message: String
+        course(id: Int!): Course
+        courses(topic: String): [Course]
+    },
+    type Mutation {
+        updateCourseTopic(id: Int!, topic: String!): Course
+    },
+
+    type Course {
+        id: Int
+        title: String
+        author: String
+        description: String
+        topic: String
+        url: String
     }
 `);
 
-// Root Resolver
-const root = {
-    message: () => 'Hello world!'
+// course
+var coursesData = [
+    {
+        id: 1,
+        title: 'The Complete Node.js Developer Course',
+        author: 'Andrew Mead, Rob Percival',
+        description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        topic: 'Node.js',
+        url: 'https://codingthesmartway.com/courses/nodejs/'
+    },
+    {
+        id: 2,
+        title: 'Node.js, Express & MongoDB Dev to Deployment',
+        author: 'Brad Traversy',
+        description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        topic: 'Node.js',
+        url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
+    },
+    {
+        id: 3,
+        title: 'JavaScript: Understanding The Weird Parts',
+        author: 'Anthony Alicea',
+        description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        topic: 'JavaScript',
+        url: 'https://codingthesmartway.com/courses/understand-javascript/'
+    }
+];
+
+getCourse = (args) => {
+    const id = args.id;
+    const result =  coursesData.filter(course => course.id === id)[0];
+    return result;
+};
+
+getCourses = (args) => {
+    if(args.topic) {
+        return coursesData.filter(course => course.topic == topic);
+    } else {
+        return coursesData;
+    }
+};
+
+updateCourseTopic = ({id, topic}) => {
+    coursesData.map(course => {
+        if(course.id === id) {
+            course.topic = topic;
+            return course;
+        }
+    });
+    return coursesData.filter(course => course.id === id) [0];
+}
+
+// Resolver
+const Resolver = {
+    message: () => 'Hello world!',
+    course: getCourse,
+    courses: getCourses,
+    updateCourseTopic: updateCourseTopic
 };
 
 // Create an express server and a GraphQL endpoint
 const app = express();
-app.use('/graphql', express_graphql({
+app.use('/graphql', graphqlHTTP({
     schema: schema,
-    rootValue: root,
+    rootValue: Resolver,
     graphiql: true
 }));
 app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
